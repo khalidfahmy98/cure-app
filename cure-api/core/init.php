0b9f3df7f210a4e967137d37fc731d70f1778f07
khalidfahmy98 <khalidahmed0551863699@gmail.com>
2020-06-07 21:25:12 +0200

    updated init routes and alert

--- cure-api/core/init.php
+++ cure-api/core/init.php
@@ -3,5 +3,4 @@ require 'core.php';
 spl_autoload_register(function($class){
     require_once "classes/" . $class . ".php";
 });
-require_once 'functions/sanitize.php';
 
--- cure-api/index.php
+++ cure-api/index.php
@@ -1,3 +1,2 @@
-<?php 
-    require 'core/init.php';
-    toasters::warning('Hello, This is not provided for user interactions or user need please back to website before unlocking your account');
+<div style="width:100%;text-align:center;font-size:50px;color:red"> Red Alert !  </div>
+<p style="width:80%;margin:auto;display:block;font-size:30px;color:gray;">  This is not provided for  user needs please back to website before unlocking your account </p>
