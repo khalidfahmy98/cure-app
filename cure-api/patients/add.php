d39e031bdd1d50cc9694c912d895c03a37e8b6f3
khalidfahmy98 <khalidahmed0551863699@gmail.com>
2020-06-07 21:37:39 +0200

    add account user api created waiting to validated request keys

--- cure-api/patients/add.php
+++ cure-api/patients/add.php
@@ -1,3 +1,27 @@
 <?php 
     require '../core/init.php';
     $patient = new patient();
+    if ( !empty(input::get('username')) && !empty(input::get('email')) && !empty(input::get('password')) ) {
+        if ( !$user->find(input::get('username')) ) {
+            $user->create(array(
+                'useremail' =>  input::get('email'), 
+                'username' =>  input::get('username'),
+                'userpassword' =>  input::get('password')
+            ) );
+            http_response_code(200);
+            echo json_encode(
+                array("message" => "Signed up successfuly")
+            );
+
+        }else{
+            http_response_code(404);
+            echo json_encode(
+                array("message" => "This user is already registered")
+            );
+        }
+    }else{
+        http_response_code(404);
+        echo json_encode(
+            array("message" => "incorrect information")
+        );
+    }
