b4fc4ff34652ac7f8c17b243ae1e7c8a3541fd82
khalidfahmy98 <khalidahmed0551863699@gmail.com>
2020-06-07 21:26:51 +0200

    alert modified

--- cure-api/index.php
+++ cure-api/index.php
@@ -1,2 +1,2 @@
-<div style="width:100%;text-align:center;font-size:50px;color:red"> Red Alert !  </div>
+<div style="width:100%;text-align:center;font-size:50px;color:red"> WARNING يا شبح  !  </div>
 <p style="width:80%;margin:auto;display:block;font-size:30px;color:gray;">  This is not provided for  user needs please back to <a href="../cure-app/"> CURE APP</a>  before unlocking your account </p>
