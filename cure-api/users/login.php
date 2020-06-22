21062d64ab4c2f80fa43ca48c64ca97858f1c4ea
khalidfahmy98 <khalidahmed0551863699@gmail.com>
2020-06-22 20:59:11 +0200

    created login api and ready

---
+++ cure-api/users/login.php
@@ -0,0 +1,24 @@
+<?php 
+    require '../core/init.php';
+    $patient = new patient();
+    if ( $patient->find(input::get('patient_username')) ) {
+
+        if($patient->login(input::get('patient_username'),input::get('patient_password'))){
+            http_response_code(200);
+            echo json_encode(
+                array("message" => "Login Success")
+            );
+        }else{
+            http_response_code(404);
+            echo json_encode(
+               array("message" => "Incorrect Information")
+           );
+        }
+
+    }else{
+        http_response_code(404);
+         echo json_encode(
+            array("message" => "Not Registered")
+        );
+    }
+
