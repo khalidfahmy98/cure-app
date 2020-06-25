9edeb6300216aff2172d12aa47cdc6f1e7223aab
khalidfahmy98 <khalidahmed0551863699@gmail.com>
2020-06-25 19:33:20 +0200

    updated info api

--- cure-api/users/info.php
+++ cure-api/users/info.php
@@ -4,10 +4,7 @@
     if ( input::get('token') == $token) {
         if ( $patient->find(input::get('patient_id')) ) {
             http_response_code(200);
-            $userInfo =  $patient->info(input::get('patient_id'));
-            
             $userArray = (array) $patient->info(input::get('patient_id'));
-            // var_dump($userArray);
             echo json_encode($userArray);
         }else{
             http_response_code(404);
