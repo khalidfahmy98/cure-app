04d2da978ddba30731616c2f71aeca8537f7d09f
khalidfahmy98 <khalidahmed0551863699@gmail.com>
2020-06-25 20:13:42 +0200

    donators_list api is created

--- cure-api/users/donators_list.php
+++ cure-api/users/donators_list.php
@@ -2,9 +2,17 @@
     require '../core/init.php';
     $patient = new patient();
     if ( input::get('token') == $token) {
-        http_response_code(200);
-        $userArray = (array) $patient->info(input::get('patient_id'));
-        echo json_encode($userArray);
+        if (input::get("donate_value") == 0   ||  input::get("donate_value") == 1) {
+            http_response_code(200);
+            $userArray = (array) $patient->ListConditionedUsers( 'patient_donation_status' , input::get("donate_value") );
+            echo json_encode($userArray);
+        }else{
+            http_response_code(404);
+            echo json_encode(array(
+                "message" => "Tokens Wrong"
+            ));
+
+        }
     }else{
         http_response_code(404);
     }
