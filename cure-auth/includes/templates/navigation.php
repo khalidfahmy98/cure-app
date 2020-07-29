b189e194491b3da94b3354939695fecc9e743f9e
khalidfahmy98 <khalidahmed0551863699@gmail.com>
2020-07-30 01:27:13 +0200

    fixed navigation logout link

--- cure-auth/includes/templates/navigation.php
+++ cure-auth/includes/templates/navigation.php
@@ -18,48 +18,6 @@
   <!-- Navbar Right Menu -->
   <div class="navbar-custom-menu">
     <ul class="nav navbar-nav">
-      <!-- Notifications: style can be found in dropdown.less -->
-      <li class="dropdown notifications-menu">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-          <i class="fa fa-bell-o"></i>
-          <span class="label label-warning">10</span>
-        </a>
-        <ul class="dropdown-menu">
-          <li class="header">You have 10 notifications</li>
-          <li>
-            <!-- inner menu: contains the actual data -->
-            <ul class="menu">
-              <li>
-                <a href="#">
-                  <i class="fa fa-users text-aqua"></i> 5 new members joined today
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa fa-warning text-yellow"></i> Very long description here that may not fit into the
-                  page and may cause design problems
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa fa-users text-red"></i> 5 new members joined
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa fa-shopping-cart text-green"></i> 25 sales made
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa fa-user text-red"></i> You changed your username
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li class="footer"><a href="#">View all</a></li>
-        </ul>
-      </li>
       <!-- User Account: style can be found in dropdown.less -->
       <li class="dropdown user user-menu">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
@@ -93,7 +51,7 @@
               <a href="#" class="btn btn-default btn-flat">Profile</a>
             </div>
             <div class="pull-right">
-              <a href="../../cure-app/logout.php" class="btn btn-default btn-flat">Sign out</a>
+              <a href="../../../cure-app/logout.php" class="btn btn-default btn-flat">Sign out</a>
             </div>
           </li>
         </ul>
