3fc64be4554df3121ff65baa3e2f24cc7b597558
khalidfahmy98 <khalidahmed0551863699@gmail.com>
2020-07-31 17:33:24 +0200

    updated cure-auth homepage starter settings

--- cure-auth/scripts/home/accountSettings.js
+++ cure-auth/scripts/home/accountSettings.js
@@ -1,18 +1,22 @@
-function saveAdmin(){
-    var xhr = new XMLHttpRequest (),
-        managerName = document.getElementById("fullname").value,
-        managerEmail = document.getElementById("email").value,
-        managerUsername = document.getElementById("username").value,
-        managerPassword = document.getElementById("password").value,
-        managerRule = document.getElementById("rule").options[document.getElementById("rule").selectedIndex].value;
-    if ( managerName !== ' ' && managerEmail !== ' ' && managerUsername !==' ' && managerPassword !== ' ' ){
-        xhr.onreadystatechange = function () {
-            if ( xhr.readyState == 4 && xhr.status == 200 ) {
-                alert( this.responseText );
-            }
-        }
-        xhr.open("POST","../../controllers/cure_managers/registerController.php",true);
-        xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-        xhr.send("manager-name="+managerName+"&manager-username="+managerUsername+"&manager-email="+managerEmail+"&manager-password="+managerPassword+"&manager-rule="+managerRule);
-    }
-}
+$(document).ready(function(){
+    
+});
+
+// function updateAccountType(accType){
+//     var xhr = new XMLHttpRequest (),
+//         managerName = document.getElementById("fullname").value,
+//         managerEmail = document.getElementById("email").value,
+//         managerUsername = document.getElementById("username").value,
+//         managerPassword = document.getElementById("password").value,
+//         managerRule = document.getElementById("rule").options[document.getElementById("rule").selectedIndex].value;
+//     if ( managerName !== ' ' && managerEmail !== ' ' && managerUsername !==' ' && managerPassword !== ' ' ){
+//         xhr.onreadystatechange = function () {
+//             if ( xhr.readyState == 4 && xhr.status == 200 ) {
+//                 alert( this.responseText );
+//             }
+//         }
+//         xhr.open("POST","../../controllers/cure_managers/registerController.php",true);
+//         xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+//         xhr.send("manager-name="+managerName+"&manager-username="+managerUsername+"&manager-email="+managerEmail+"&manager-password="+managerPassword+"&manager-rule="+managerRule);
+//     }
+// }
--- cure-auth/views/home/index.php
+++ cure-auth/views/home/index.php
@@ -14,28 +14,44 @@
         ?>
           <div class="alert alert-info">Register Now Your Own Orgnization Or Work ! </div>
             <div class="row">
-              <div class="col-md-3 col-sm-6 col-xs-12">
-                <div class="info-box">
-                  <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
-                  <div class="info-box-content">
-                    <span class="info-box-text">Service Provider </span>
-                  </div>
-                  <!-- /.info-box-content -->
+                <div class="col-md-3 col-sm-4 col-xs-6 form-group">
+                    <label>Work Type : </label>
+                    <select class="form-control input-sm">
+                        <option value="1">Service Provider</option>
+                        <option value="2">Doctor </option>
+                    </select>
                 </div>
-                <!-- /.info-box -->
-              </div>
-              <!-- /.col -->
-              <div class="col-md-3 col-sm-6 col-xs-12">
-                <div class="info-box">
-                  <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
-                  <div class="info-box-content">
-                    <span class="info-box-text">Doctor or Pharamciest</span>
-                  </div>
-                  <!-- /.info-box-content -->
+                <div class="col-md-3 col-sm-4 col-xs-6 form-group">
+                    <label>Orgnization Name :  </label>
+                    <input type="text" class="form-control input-sm">
                 </div>
-                <!-- /.info-box -->
-              </div>
-              <!-- /.col -->
+                <div class="col-md-3 col-sm-4 col-xs-6 form-group">
+                    <label>Orgnization Type :  </label>
+                    <input type="text" class="form-control input-sm">
+                </div>
+                <div class="col-md-3 col-sm-4 col-xs-6 form-group">
+                    <label>Orgnization Email :  </label>
+                    <input type="text" class="form-control input-sm">
+                </div>
+                <div class="col-md-3 col-sm-4 col-xs-6 form-group">
+                    <label>Orgnization Phone :  </label>
+                    <input type="text" class="form-control input-sm">
+                </div>
+                <div class="col-md-3 col-sm-4 col-xs-6 form-group">
+                    <label>License Number :  </label>
+                    <input type="text" class="form-control input-sm">
+                </div>
+                <div class="col-md-3 col-sm-4 col-xs-6 form-group">
+                    <label>City :  </label>
+                    <input type="text" class="form-control input-sm">
+                </div>
+                <div class="col-md-3 col-sm-4 col-xs-6 form-group">
+                    <label>Address :  </label>
+                    <input type="text" class="form-control input-sm">
+                </div>
+            </div>
+            <div class="form-group">
+              <button class="btn btn-sm btn-success">Create Orgniztion</button>
             </div>
         <?php 
       }else{
