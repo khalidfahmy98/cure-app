b99e062674fec90f517b2f7af4976183ff897508
khalidfahmy98 <khalidahmed0551863699@gmail.com>
2020-08-05 08:58:02 +0200

    connected and plugged in the cure portfolio into the app

---
+++ index.php
@@ -0,0 +1,394 @@
+<!DOCTYPE html>
+<html >
+<head>
+   <meta charset="utf-8" />
+   <!--First Mobile Meta-->
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--End Mobile Meta-->
+   <title>Cure Portfolio</title>
+   <!--Start Fontawesome-->
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+      crossorigin="anonymous">
+   <!--End Fontawesome-->
+   <!--Start Google Font-->
+   <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Tangerine">
+   <!--End Google Font-->
+      <!--Start Bootstrap Css-->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <!--End Bootstrap Css-->
+   <link rel="stylesheet" href="assets/css/portfolio.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+</head>
+<body>
+    
+    <!--Start Cure Portfolio-->
+      <!--Start Navbar-->
+        <div class="portfolio">
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container">
+          <div>
+            <img class="cure-logo" src="assets/imgs/logo.png.png"/>
+          </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Faq</a>
+              </li>
+            </ul>
+          </div>
+          </div>
+        </nav>
+        </div>
+      <!--End Navbar-->
+      <div class="port">
+        <div class="container">
+          <div class="row">
+            <div class="port-h2 col-md-12">
+              <h2>Hello From Cure Platform This Website Help Our Paitent To Be A Good</h2>
+              <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+              <button type="button" class="active btn btn-outline">Submit</button>
+              <button type="button" class="btn btn-outline">Log Out</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!--End Cure Portfolio-->
+    <!--Start About "-->
+      <div class="about text-center">
+        <h1>Meet Me Our New Templete</h1>
+        <p class="lead">Hello Let Me Introduce Our New Templete. Created With Me <strong>Cure</strong> Platform</p>
+      </div>
+    <!--End About-->
+    <!--Start Features-->
+    <div class="features text-center">
+      <div class="container">
+        <h1>Our Features</h1>
+        <div class="row">
+          <div class="col-lg-3 col-md-6">
+            <div class="feat">
+            <i class="fab fa-amazon-pay fa-3x"></i>
+            <h4>100% Responsive</h4>
+            <p>This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="feat">
+            <i class="fab fa-font-awesome-flag fa-3x"></i>
+            <h4>Font Awesome</h4>
+            <p>This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="feat">
+              <i class="fab fa-atlassian fa-3x"></i>
+            <h4>Features</h4>
+            <p>This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="feat">
+              <i class="fab fa-battle-net fa-3x"></i>
+            <h4>Well Documented</h4>
+            <p>This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph This IS Our Pargraph</p>
+          </div>  
+        </div>
+        </div>
+      </div>
+    </div>
+    <!--End Features-->
+    <!--Start Testimonials Carousal-->
+    <div class="testimonials text-center">
+      <div class="container">
+      <h1>What Our Clients Say</h1>
+      <!--Start Carousel Testimonials-->
+    <div class="bd-example">
+      <div id="carousel-testimonials" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <span>Tarek NeGm</span>
+          </div>
+          <div class="carousel-item">
+              <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <span>Menam Omar</span>
+            </div>
+          <div class="carousel-item">
+              <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <span>Khalid Fahmy</span>
+            </div>
+          <div class="carousel-item">
+              <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <span>Ahmed Samy</span>
+            </div>
+        </div>
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-testimonials" data-slide-to="0" class="active">
+                <img src="./assets/imgs/avatar1.jpg">
+            </li>
+            <li data-target="#carousel-testimonials" data-slide-to="1">
+                <img src="./assets/imgs/avatar2.jpg">   
+            </li>
+            <li data-target="#carousel-testimonials" data-slide-to="2">
+                <img src="./assets/imgs/avatar3.jpg">
+            </li>
+            <li data-target="#carousel-testimonials" data-slide-to="3">
+                <img src="./assets/imgs/avatar4.jpg">
+            </li>
+        </ol>
+      </div>
+    </div>
+  <!--End Carousel Testimonials-->
+</div>
+</div>
+    <!--End Testimonials Carousal-->
+    <!--Start Pricing Table-->
+   <div class="pricing-table text-center">
+    <div class="container">
+       <h2>Pricing Table</h2>
+       <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s Lorem Ipsum has been the industry's standard dummy text ever since the 1500s Lorem Ipsum has been the industry's standard dummy text ever since the 1500s Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley</p>
+    <div class="row">
+    <!--Start Card-->
+    <div class="card col-lg-4 col-sm-12">
+    <div class="card-body">
+      <h5 class="card-title">Start Business</h5>
+      <h6 class="card-subtitle mb-2 text-muted">July 12 2019</h6>
+      <p>99$/<span>Year</span></p>
+      <ul class="list-group list-group-flush">
+          <li class="list-group-item">Lorem Ipsum</li>
+          <li class="list-group-item">Lorem Ipsum</li>
+          <li class="list-group-item">Lorem Ipsum</li>
+          <li class="list-group-item">Lorem Ipsum</li>
+          <li class="list-group-item">Lorem Ipsum</li>               
+        </ul>
+      <a href="#" class="card-link">Read More</a>
+    </div>
+    </div>
+       <!--End Card-->
+           <!--Start Card-->
+       <div class="card corporate col-lg-4 col-sm-12">
+             <div class="card-body">
+               <h5 class="card-title">Corporate</h5>
+               <h6 class="card-subtitle mb-2 text-muted">July 12 2019</h6>
+               <p>199$/<span>Year</span></p>
+               <ul class="list-group list-group-flush">
+                   <li class="list-group-item">Lorem Ipsum</li>
+                   <li class="list-group-item">Lorem Ipsum</li>
+                   <li class="list-group-item">Lorem Ipsum</li>
+                   <li class="list-group-item">Lorem Ipsum</li>
+                   <li class="list-group-item">Lorem Ipsum</li>               
+                 </ul>
+               <a href="#" class="card-link">Read More</a>
+             </div>
+           </div>
+           <!--End Card-->
+           <!--Start Card-->
+       <div class="card col-lg-4 col-sm-12">
+             <div class="card-body">
+               <h5 class="card-title">Enterprise</h5>
+               <h6 class="card-subtitle mb-2 text-muted">July 12 2019</h6>
+               <p>299$/<span>Year</span></p>
+               <ul class="list-group list-group-flush">
+                   <li class="list-group-item">Lorem Ipsum</li>
+                   <li class="list-group-item">Lorem Ipsum</li>
+                   <li class="list-group-item">Lorem Ipsum</li>
+                   <li class="list-group-item">Lorem Ipsum</li>
+                   <li class="list-group-item">Lorem Ipsum</li>               
+                 </ul>
+               <a href="#" class="card-link">Read More</a>
+             </div>
+           </div>
+           <!--End Card-->
+        </div>
+        </div>
+    </div>
+    <!--End Pricing Table-->
+    <!--Start Why Choose Us-->
+    <div class="choose-us">
+    <div class="container-fluid">
+    <div class="row">
+      <div class="info col-md-6">
+        <img src="./assets/imgs/choose.jpg">
+      </div>
+      <div class="info col-md-6">
+        <h2 class="h1">Why Choose Us</h2>
+        <p>Ipsum has been the industry's standard dummy text ever since the 1500s Lorem Ipsum has been the industry's standard dummy text ever since the 1500s Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley</p>
+        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley</p>
+        <a href="#" class="read-more">Read More</a>
+          </div>
+          </div>
+          </div>
+        </div>
+      <!--End Why Choose Us-->  
+      <!--Start Statistics-->
+      <div class="stats text-center">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 col-lg-3">
+              <div class="stat-box">
+            <i class="fas fa-user-tie fa-fw fa-5x"></i>
+            <span>624</span>
+            <p>Happy Clients</p>
+          </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+              <div class="stat-box">
+            <i class="fas fa-heart fa-fw fa-5x"></i>
+              <span>112</span>
+            <p>Amazing Tours</p>
+          </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+              <div class="stat-box">
+            <i class="fas fa-briefcase fa-fw fa-5x"></i>
+            <span>595</span>
+            <p>partiners</p>
+          </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+              <div class="stat-box">
+            <i class="fas fa-comment fa-fw fa-5x"></i>
+            <span>09</span>
+            <p>Questions Answered</p>
+          </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--End Statistics-->
+      <!--Start Contact Us-->
+    <div class="contact-us text-center">
+      <div class="field">
+        <div class="container">
+          <i class="fas fa-headphones fa-5x"></i>
+          <h1>Tell Us What You Fail</h1>
+          <p class="lead">Fail Free Contact Us AnyTime</p>
+      <!--Start Form-->
+      <form role="form ">
+        <div class="row">
+          <div class="col-md-6">
+          <div class="form-group">
+          <input class="form-control form-control-lg" type="text" placeholder="Username"/>
+          </div>
+          <div class="form-group">
+            <input class="form-control form-control-lg" type="text" placeholder="Email"/>
+            </div>
+            <div class="form-group">
+          <input class="form-control form-control-lg" type="text" placeholder="Cell Phone"/>
+          </div>
+        </div>
+        </form>
+        <div class="col-md-6">
+            <div class="form-group">
+              <textarea class="form-control form-control form-control-lg" placeholder="Your Message"></textarea>
+            </div>
+            <button class="btn btn-danger btn-lg btn-block">contact Us</button>
+          </div>
+        </div>
+      <!--End Form-->
+        </div>
+      </div>
+    </div>
+    <!--End Contact Us-->
+    <!--Start Footer-->
+   <div class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="site-info col-md-6">
+         <span>Cu</span><span>Re</span>
+         <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s Lorem Ipsum has been the industry's standard dummy text ever since the 1500s Lorem Ipsum has been the industry's standard dummy text ever since the 1500s Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley</p>
+         <a href="#">Read More</a>
+        </div>
+        <div class="helpful-links col-md-3">
+         <h2>Helpful Links</h2>
+         <div class="row">
+         <div class="col">
+         <ul class="list-unstyled">
+           <li>About</li>
+           <li>Portofolio</li>
+           <li>Team</li>
+           <li>Price</li>
+           <li>Privacy</li>
+         </ul>
+       </div>
+       <div class="col">
+         <ul class="list-unstyled">
+           <li>Faq</li>
+           <li>Blog</li>
+           <li>How It Work</li>
+           <li>Benefits</li>
+           <li>Contact</li>
+         </ul>
+       </div>
+       </div>
+        </div>
+        <div class="contact col-md-3">
+         <h2>Contact Us</h2>
+         <ul class="list-unstyled">
+           <li>Location : Zagazig</li>
+           <li>Phone : 01061331755</li>
+           <li>Email : <a href="mailto:tm76498@gmail.com?subject=content">tm76498@gmail.com</a></li>
+         </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--End Footer-->
+  <!--Start Copyright-->
+  <div class="copyright">
+    <div class="container">
+      <div class="row">
+        <div class="write-copyright col-sm-6 text-center text-md-left text-uppercase">
+         Copyright 8/4/2020 &copy; Cure All Right Reserverd
+        </div>
+        <div class="col-sm-6 text-center text-md-right">
+          <ul class="list-unstyled">
+           <li>
+           <a href="#">
+             <i class="fab fa-facebook"></i>
+           </a>
+         </li>
+         <li>
+             <a href="#">
+               <i class="fab fa-twitter"></i>
+             </a>
+           </li>
+           <li>
+               <a href="#">
+                 <i class="fab fa-youtube"></i>
+               </a>
+             </li>
+             <li>
+                 <a href="#">
+                   <i class="fab fa-google"></i>
+                 </a>
+               </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--End Copyright-->
+
+   <script src="http://code.jquery.com/jquery-1.11.1.js" integrity="sha256-MCmDSoIMecFUw3f1LicZ/D/yonYAoHrgiep/3pCH9rw="
+   crossorigin="anonymous"></script>
+   <script src="assets/js/main.js"></script>
+   <!--Start Bootstrap Js-->
+   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+   <!--End Loading By JQuery--> 
+</body>
+</html>
